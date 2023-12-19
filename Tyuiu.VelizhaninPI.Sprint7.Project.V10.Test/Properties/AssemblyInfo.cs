e564598f65401b1229c79f6b11332c0a5194d7bb
VelizhaninPavel <velizhaninpavel@gmail.com>
2023-12-19 23:38:44 +0500

    Add project files.

---
+++ Tyuiu.VelizhaninPI.Sprint7.Project.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VelizhaninPI.Sprint7.Project.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.VelizhaninPI.Sprint7.Project.V10.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d86004e-486b-4601-82a1-dd515e8b3cc0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.VelizhaninPI.Sprint7.Project.V10/FormMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Tyuiu.VelizhaninPI.Sprint7.Project.V10.FormMainProject.Form1;
+
+namespace Tyuiu.VelizhaninPI.Sprint7.Project.V10
+{
+    public partial class FormMainProject : Form
+    {
+        public FormMainProject()
+        {
+            InitializeComponent();
+
+            var attribute = DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE;
+            var preference = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND;
+            DwmSetWindowAttribute(this.Handle, attribute, ref preference, sizeof(uint));
+
+            //openFileDialogTask_VPI.Filter = "Значения, разделённые запятыми(*.csv)|*.csv|Все файлы(*.*)|*.*";
+            //saveFileDialogTask_VPI.Filter = "Значения, разделённые запятыми(*.csv)|*.csv|Все файлы(*.*)|*.";
+        }
+
+
+        public partial class Form1 : Form
+        {
+            public enum
+            DWMWINDOWATTRIBUTE
+            {
+                DWMWA_WINDOW_CORNER_PREFERENCE = 33
+            }
+
+            public enum
+            DWM_WINDOW_CORNER_PREFERENCE
+            {
+                DWMWCP_DEFAULT = 0,
+                DWMWCP_DONOTROUND = 1,
+                DWMWCP_ROUND = 2,
+                DWMWCP_ROUNDSMALL = 3
+            }
+
+            [DllImport("dwmapi.dll", CharSet = CharSet.Unicode, PreserveSig = false)]
+            internal static extern void
+            DwmSetWindowAttribute
+            (
+            IntPtr hwnd, DWMWINDOWATTRIBUTE attribute, ref DWM_WINDOW_CORNER_PREFERENCE pvAttribute, uint cbAttribute);
+
+        }
+    }
+}
